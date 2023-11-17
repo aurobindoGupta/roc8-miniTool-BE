02a03538e334cc3609cc6f707b6d8108cc22aae9
AurobindoGupta <aurogpt10@gmail.com>
2023-11-17 17:25:03 +0530

    remove.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const OpenAI = require("openai");
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
 
